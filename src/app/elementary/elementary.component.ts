7b7c7a0b6c9a291c9eaa3d102580f051aff439e6
Chiki-Paperoni <tv81eugene@gmail.com>
2021-10-28 18:10:07 +0300

    schoool gallery changed

--- src/app/elementary/elementary.component.ts
+++ src/app/elementary/elementary.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElementaryComponent implements OnInit {
   list: string[] = [
-    'elementary/gallery/1.jpg',
+    'elementary/gallery/1.png',
     'elementary/gallery/2.png',
-    'elementary/gallery/3.png',
+    'elementary/gallery/3.jpg',
     'elementary/gallery/4.png',
     'elementary/gallery/5.png',
     'elementary/gallery/6.jpg',
